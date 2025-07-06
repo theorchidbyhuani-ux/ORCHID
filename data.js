d55ba4205b299c528eb632012274a0825a046f4a
Ruban Singh <99957125+lykruban@users.noreply.github.com>
2025-07-06 16:19:30 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,11 @@
+const globalPromoData = {};
+const globalServicesData = {
+    eyelash: [],
+    nails: [],
+    massage: [],
+    facial: []
+};
+const globalGalleryData = {
+    gallery: [],
+    services: []
+}; 
